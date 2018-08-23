15fa52c1f5ebc46b67e86a6c385cc180e6663c2f
Sarah Pai <pai.sarah89@gmail.com>
2018-08-23 00:02:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,13 +3,6 @@ function happyHolidays(){
 }
 
 function happyHolidaysTo(name){
+  name ("you");
   return `Happy holidays, ${name}!`
-  }
-  
-  function happyHolidaysTo(holiday, name){
-    return `Happy ${holiday}, ${name}!`
-  }
-  
-  function holidayCountdown(holidayName, numberOfDays){
-    return `It's ${days}, days until ${holiday}!`
   }
